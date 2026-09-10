9290a7534be43af475a25a7c45bae64e48bb5a43
monchozsramdhan <monchozs.ramdhan@gmail.com>
2026-09-10 10:26:15 +0700

    Add files via upload

---
+++ branding.js
@@ -0,0 +1,8 @@
+window.pypBranding = Object.freeze({
+  businessName: 'pypcoffee',
+  logoSrc: 'pyp-logo.png',
+  logoAlt: 'pypcoffee logo',
+  tagline: 'Table reservation',
+  primaryColor: '#186F4E',
+  whatsappNumber: '6287728730505'
+});
