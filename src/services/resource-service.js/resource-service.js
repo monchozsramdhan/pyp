f0a04ef0133701afbb0e8c5b3bfc2c7efe46ce7f
monchozsramdhan <monchozs.ramdhan@gmail.com>
2026-09-10 10:30:58 +0700

    Delete src/services/resource-service.js/resource-service.js

--- src/services/resource-service.js/resource-service.js
+++
@@ -1,9 +0,0 @@
-window.pypResourceService = Object.freeze({
-  listTables(data) {
-    return window.pypLegacyStorage.previewResources(data);
-  },
-
-  findTable(data, id) {
-    return this.listTables(data).find(resource => resource.legacyId === Number(id));
-  }
-});
